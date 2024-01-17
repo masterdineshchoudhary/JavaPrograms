e7549e3ab18999e1cae086b26f4613fc7d08a1e8
masterdineshchoudhary <masterdineshchoudhary@gmail.com>
2024-01-17 19:54:45 +0530

    Number Pattern 2

--- src/pattern/NumberPattern2.java
+++ src/pattern/NumberPattern2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 // Pyramid of Numbers
 
-public class Pattern2 {
+public class NumberPattern2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("How many rows");
