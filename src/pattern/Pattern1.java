56b67cf104609e2e36affe9dfe2cb9ce463bc964
masterdineshchoudhary <masterdineshchoudhary@gmail.com>
2024-01-17 19:36:52 +0530

    Number Pattern 1

--- src/pattern/Pattern1.java
+++ src/pattern/Pattern1.java
@@ -2,7 +2,7 @@ package pattern;
 
 import java.util.Scanner;
 
-// package Pyramid of Numbers;
+// Pyramid of Numbers
 
 public class Pattern1 {
     public static void main(String[] args) {
@@ -11,13 +11,16 @@ public class Pattern1 {
         int noOfRows = sc.nextInt();
         int rowCount = 1;
         for (int i = noOfRows; i > 0 ; i--) {
+        	// printing i spaces at beginning of each row
             for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
             }
+            // printing rowCount value rowCount times at the end of each row
             for (int j = 1; j <= rowCount; j++) {
                 System.out.print(rowCount+" ");
             }
             System.out.println();
+            // incrementing rowCount
             rowCount++;
             sc.close();
         }
