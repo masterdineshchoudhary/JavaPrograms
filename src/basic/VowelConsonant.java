d272bd7d3d6d32fdcedf1ee2d0f6641a5ef08336
masterdineshchoudhary <masterdineshchoudhary@gmail.com>
2024-01-28 08:52:56 +0530

    Aplhabet is Vowel or Consonant

---
+++ src/basic/VowelConsonant.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class VowelConsonant {
+	public static void main(String[] args) {
+		char ch = 'a';
+		if (ch == 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+			System.out.println("Is a Vowel");
+		} else {
+			System.out.println("Is a Consonant");
+		}
+	}
+}
